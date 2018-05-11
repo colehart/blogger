5818549c4effac381fca6a9fbe20e8be4c5397db
Cole Hart <cole@colehart.com>
2018-05-10 18:13:35 -0600

    added all articles feature spec

---
+++ spec/features/user_sees_all_articles_spec.rb
@@ -0,0 +1,16 @@
+# feature test - articles controller
+require 'rails_helper'
+
+describe 'user sees all articles' do
+  scenario 'they visit /articles' do
+    it 'displays all articles' do
+      article1 = Article.create!(title: 'Title 1', body: 'Body 1')
+      article2 = Article.create!(title: 'Title 2', body: 'Body 2')
+
+      visit '/articles'
+
+      expect(page).to have_content(article1.title)
+      expect(page).to have_content(article2.title)
+    end
+  end
+end
