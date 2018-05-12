0b9b4804e072085f8efba0d9307397ea40016ad0
Cole Hart <cole@colehart.com>
2018-05-12 12:17:26 -0600

    added create new article link'

--- spec/features/user_sees_all_articles_spec.rb
+++ spec/features/user_sees_all_articles_spec.rb
@@ -6,11 +6,13 @@ describe 'user sees all articles' do
     it 'displays all articles' do
       article1 = Article.create!(title: 'Title 1', body: 'Body 1')
       article2 = Article.create!(title: 'Title 2', body: 'Body 2')
+      new_article_button = 'Create a New Article'
 
       visit '/articles'
 
       expect(page).to have_link(article1.title)
       expect(page).to have_link(article2.title)
+      expect(page).to have_link(new_article_button)
     end
   end
 end
